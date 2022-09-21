8c2e1d691f99969b057333c3d82ecd447c62df55
Jose Maria Flores Zazo <jmfloreszazo@gmail.com>
2022-09-21 15:04:22 +0200

    Update project

--- testOnLinux/Program.cs
+++ testOnLinux/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int x = 12345;
+int y = DoSomeWork();
+Console.WriteLine($"X Value is {x}");
+Console.WriteLine($"Y Value is {y}");
+
+static int DoSomeWork()
+{
+    int y = 98765;
+    Console.WriteLine("Please enter key to show variables values allocated in memory...");
+    Console.ReadKey();
+    return y;
+}
